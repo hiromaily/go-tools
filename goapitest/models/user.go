cae5487a7751d26dddba9b524195e35d90090856
hiromaily <hiromaily2@gmail.com>
2017-06-27 18:11:54 +0200

    delete comment out

--- goapitest/models/user.go
+++ goapitest/models/user.go
@@ -6,41 +6,6 @@ import (
 	"time"
 )
 
-//curl 'http://localhost:3000/api/data/User?_dc=1498290488694'
-// -H 'Cookie: gearbox=s%3AR8cD0biFGWAxk4lXCPahYMEXqdFT0k98.S8idxIcHAeqIBs7QdCwThfbIQ6DgSHj5p6b2D3xaHwU'
-// -H 'Origin: http://localhost:3000'
-// -H 'Accept-Encoding: gzip, deflate, br'
-// -H 'Accept-Language: en-US,en;q=0.8,ja;q=0.6,nl;q=0.4,de;q=0.2,fr;q=0.2'
-// -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
-// -H 'Content-Type: application/json'
-// -H 'Accept: */*'
-// -H 'Referer: http://localhost:3000/'
-// -H 'X-Requested-With: XMLHttpRequest'
-// -H 'Connection: keep-alive'
-// --data-binary '[{"id":"7974d21a-64aa-4b44-a550-3d830891cac9","name":"username","password_last_changed":null,"password_expiration":null,"attr":null,"type":"full","email":"email@test.com","gender":"Male","enabled":true,"comments":"Comment something","password":"H2&t3#I1","last_name":"lastname","first_name":"firstname","dashboard_id":null,"password_expired":false,"send_password_email":true,"departments":[],"teams":[],"roles":["55a6e956-2d61-4a68-91e0-d51b194dd700"]}]'
-// --compressed
-
-// --data-binary '[{
-// "id":"7974d21a-64aa-4b44-a550-3d830891cac9",
-// "name":"username",
-// "first_name":"firstname",
-// "last_name":"lastname",
-// "password":"H2&t3#I1",
-// "password_last_changed":null,
-// "password_expiration":null,
-// "password_expired":false,
-// "send_password_email":true,
-// "email":"email@test.com",
-// "gender":"Male",
-// "type":"full",
-// "enabled":true,
-// "dashboard_id":null,
-// "comments":"Comment something",
-// "attr":null,
-// "departments":[],
-// "teams":[],
-// "roles":["55a6e956-2d61-4a68-91e0-d51b194dd700"]}]'
-
 type User struct {
 	Id                  string     `json:"id"`
 	Name                string     `json:"name"`
